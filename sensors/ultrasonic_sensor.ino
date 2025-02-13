1261e728881d7b7ae7164bfe44968acbe973e147
Anish Chakraborty <115884117+AnishC10@users.noreply.github.com>
2025-02-13 17:42:33 -0500

    Update ultrasonic_sensor.ino

--- sensors/ultrasonic_sensor.ino
+++ sensors/ultrasonic_sensor.ino
@@ -1,27 +1,20 @@
-int trig = 9;
-int echo = 8;
-int distance = 0;
-long duration =0;
-void setup() {
-  // put your setup code here, to run once:
-  pinMode(trig, OUTPUT);
-  pinMode(echo, INPUT);
-  Serial.begin(9600);
- 
-}
+#define TRIG_PIN 9
+#define ECHO_PIN 8;
 
-void loop() {
-  // put your main code here, to run repeatedly:
- 
+
+long distance(){
   digitalWrite(trig, LOW);
   delayMicroseconds(2);
   digitalWrite(trig, HIGH);
   delayMicroseconds(10);
   digitalWrite(trig, LOW);
-  duration = pulseIn(echo, HIGH);
-  distance = (duration *0.034)/2;
-  Serial.print(distance);
-  Serial.println(" cm");
-  delay(1000);
-   
+  long duration = pulseIn(echo, HIGH);
+  long distance = (duration *0.034)/2;
+  return distance;
+}
+
+void avoidObstacle() {
+    if (distance() < 10) {  // 10cm
+        stopMotors();
+    }
 }
